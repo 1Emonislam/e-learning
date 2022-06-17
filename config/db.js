eb23ce170fe1b8857b7fab0cde8a5416df7a0762
1emonislam <emonislamweb@gmail.com>
2022-06-17 18:10:28 +0600

    all code connected db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB =async () => {
     try {
-       await mongoose.connect('mongodb://localhost:27017/e-learing');
+       await mongoose.connect(process.env.MONGO_URI);
        console.log('server connected Mongodb')
     }
     catch (error) {
